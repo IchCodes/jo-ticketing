598797fd806513b9e04f646a2af4984fc948d4fa
IchCodes <hichame.sraidi@gmail.com>
2024-04-28 19:35:09 +0200

    Début de projet

---
+++ src/main/java/com/studi/joticketing/JoTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.studi.joticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoTicketingApplication.class, args);
+	}
+
+}
