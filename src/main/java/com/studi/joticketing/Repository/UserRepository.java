f5a246ef73c665c75a012c82817535d632f54fd2
IchCodes <hichame.sraidi@gmail.com>
2024-05-02 23:02:18 +0200

    Basic User Repo

---
+++ src/main/java/com/studi/joticketing/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.studi.joticketing.Repository;
+
+import com.studi.joticketing.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+}
