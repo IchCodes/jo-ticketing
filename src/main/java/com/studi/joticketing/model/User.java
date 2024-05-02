086dd04801a4fdd9538a5360bba7bfec364b6a5a
IchCodes <hichame.sraidi@gmail.com>
2024-05-02 23:01:46 +0200

    User Model

---
+++ src/main/java/com/studi/joticketing/model/User.java
@@ -0,0 +1,36 @@
+package com.studi.joticketing.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "email")
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    private String user_key;
+
+}
