df60b0fc1720e8a43f9f8ccd7cd279c04f7c3a6a
IchCodes <hichame.sraidi@gmail.com>
2024-05-02 23:02:36 +0200

    Custom user response by DTO

---
+++ src/main/java/com/studi/joticketing/DTO/UserResponse.java
@@ -0,0 +1,17 @@
+package com.studi.joticketing.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private String lastName;
+    private String firstName;
+    private String role;
+    private String customField1;
+}
