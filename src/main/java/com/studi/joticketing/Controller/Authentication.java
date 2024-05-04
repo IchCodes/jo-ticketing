7cd37b0c87ba499456cf714a50a4c822555e6b66
IchCodes <hichame.sraidi@gmail.com>
2024-05-04 02:30:17 +0200

    /api/v1/auth/register implemented

--- src/main/java/com/studi/joticketing/Controller/Authentication.java
+++ src/main/java/com/studi/joticketing/Controller/Authentication.java
@@ -1,16 +1,25 @@
 package com.studi.joticketing.Controller;
 
+import com.studi.joticketing.DTO.UserResponse;
+import com.studi.joticketing.Service.UserService;
+import com.studi.joticketing.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class Authentication {
 
+    private final UserService userService;
+
     @PostMapping("/register")
-    public String register() {
-        return "Register";
+    public ResponseEntity<String> register(@RequestBody UserResponse userResponse) {
+        return ResponseEntity.ok(userService.saveUser(userResponse));
     }
 
     @PostMapping("/login")
--- src/main/java/com/studi/joticketing/Service/UserService.java
+++ src/main/java/com/studi/joticketing/Service/UserService.java
@@ -3,8 +3,11 @@ package com.studi.joticketing.Service;
 import com.studi.joticketing.DTO.UserResponse;
 import com.studi.joticketing.Mapper.UserMapper;
 import com.studi.joticketing.Repository.UserRepository;
+import com.studi.joticketing.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +25,19 @@ public class UserService {
         return userRepository.findByUsername(username)
                 .map(UserMapper::toUserResponse)
                 .orElseThrow();
+    }
+
+    public String saveUser(UserResponse userResponse) {
+        User user = User.builder()
+                .firstName(userResponse.getFirstName().substring(0, 1).toUpperCase() + userResponse.getFirstName().substring(1).toLowerCase())
+                .lastName(userResponse.getLastName().toUpperCase())
+                .username(userResponse.getUsername())
+                .password(userResponse.getPassword())
+                .role("USER")
+                .user_key(UUID.randomUUID().toString())
+                .build();
+         userRepository.save(user);
+
+         return "User "+ user.getLastName() + " " + user.getFirstName() + " created successfully";
     }
 }
